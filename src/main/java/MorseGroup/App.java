fda231c3807ed54a3ce46342ad02c6c25a2d0e9e
aborysionek <adrianborysionek@gmail.com>
2017-01-09 20:02:37 +0100

    dodana metoda code

--- src/main/java/MorseGroup/App.java
+++ src/main/java/MorseGroup/App.java
@@ -1,5 +1,9 @@
 package MorseGroup;
 
+import sun.awt.AWTCharset;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,6 +13,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Morse code - input text:" );
+        Scanner input = new Scanner(System.in);
+        new Code(input);
+        new Decode;
+        new File;
     }
 
 }
