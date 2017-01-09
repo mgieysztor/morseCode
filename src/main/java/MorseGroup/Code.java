29a5acb316e964111dcfe2c7821a926267028da8
aborysionek <adrianborysionek@gmail.com>
2017-01-09 19:38:45 +0100

    dodana metoda code

---
+++ src/main/java/MorseGroup/Code.java
@@ -0,0 +1,10 @@
+package MorseGroup;
+
+/**
+ * Created by RENT on 2017-01-09.
+ */
+public class Code {
+    public void Coding(String klawiatura){
+        System.out.println(klawiatura);
+    }
+}
